a26b9f0ed43937b4d07e10a584ea649a9e04b866
h1divp <71522316+h1divp@users.noreply.github.com>
2023-12-20 00:56:05 -0500

    Added new parameters to Post type. Added PUT logic for stars, title, isLocked and tag updates

--- server/src/types/Post.ts
+++ server/src/types/Post.ts
@@ -2,7 +2,11 @@ export interface Post {
     postId: string
     authorId: string
     frogUrl: string
+    title: string
+    tags: string
+    isLocked: boolean
     timeCreated: number
+    timeUpdated: number
     star1: number
     star2: number
     star3: number
